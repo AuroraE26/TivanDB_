fcfb8d8a6d700438dd51bf4d274127e675e404aa
Ftnt <jairo.rocano@gmail.com>
2022-02-17 00:54:38 -0500

    add package morgan and change variable PORT for app.set port

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 const express = require("express");
+const morgan = require("morgan");
 const cors = require("cors");
 
 require("dotenv").config();
-const productRoutes = require("./routes/product.routes"); 
-const salesRoutes = require("./routes/sales.routes"); 
-const detailsRoutes = require("./routes/details.routes"); 
+const productRoutes = require("./routes/product.routes");
+const salesRoutes = require("./routes/sales.routes");
+const detailsRoutes = require("./routes/details.routes");
+
+//Setting
+app.set("port", process.env.TIVAN_API_PORT || 3001);
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/api/productos', productRoutes);
-app.use('/api/sales', salesRoutes);
-app.use('/api/details', detailsRoutes);
+app.use("/api/productos", productRoutes);
+app.use("/api/sales", salesRoutes);
+app.use("/api/details", detailsRoutes);
 
-const PORT = process.env.TIVAN_API_PORT;
-app.listen(PORT, ()=>{
-    console.log(`run serven in port ${PORT}`);
+app.listen(port, () => {
+  console.log(`run serven in port ${port}`);
 });
-
