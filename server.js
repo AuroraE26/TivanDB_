c354027894a6288b4d0dd5845f15c14b9365f24f
Ftnt <jairo.rocano@gmail.com>
2022-02-17 01:02:32 -0500

    modify structure for server.js

--- server.js
+++ server.js
@@ -7,16 +7,21 @@ const productRoutes = require("./routes/product.routes");
 const salesRoutes = require("./routes/sales.routes");
 const detailsRoutes = require("./routes/details.routes");
 
-//Setting
+const app = express();
+
+//setting
 app.set("port", process.env.TIVAN_API_PORT || 3001);
 
-const app = express();
+//middleware
+app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
+
+//routes
 app.use("/api/productos", productRoutes);
 app.use("/api/sales", salesRoutes);
 app.use("/api/details", detailsRoutes);
 
-app.listen(port, () => {
-  console.log(`run serven in port ${port}`);
+app.listen(app.get("port"), () => {
+  console.log(`Server running in port ${app.get("port")}`);
 });
