26c9f2bae31297bbba225f14434d1c8bec99d48e
Ftnt <jairo.rocano@gmail.com>
2022-02-21 03:48:58 -0500

    add config for upload images in bucket aws

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -1,5 +1,8 @@
 const Product = require("../usercase/product.case");
 
+const upload = require("../lib/common");
+const { uploadFile } = require("../lib/s3");
+
 exports.create = (req, res) => {
   if (!req.body) {
     res.status(400).send({
@@ -157,3 +160,24 @@ exports.pieces = (req, res) => {
     } else res.send(data);
   });
 };
+
+exports.uploadImage=()=>{
+  upload.single("image"), async (req, res) => {
+    console.log(req.file);
+  
+    // uploading to AWS S3
+    const result = await uploadFile(req.file);
+    console.log("S3 response", result);
+  
+    // You may apply filter, resize image before sending to client
+  
+    // Deleting from local if uploaded in S3 bucket
+    await unlinkFile(req.file.path);
+  
+    res.send({
+      status: "success",
+      message: "File uploaded successfully",
+      data: req.file,
+    });
+  };
+} 
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,6 +1,8 @@
 const products = require("../controller/product.controller.js");
 var router = require("express").Router();
 
+
+
     router.post("/", products.create);
     router.get("/", products.findAll);
     router.get("/:id", products.findOne);
@@ -8,5 +10,7 @@ var router = require("express").Router();
     router.patch("/:id",products.logicDelete);
     router.patch("/:id/favorito",products.favorite);
     router.patch("/:id/piece",products.pieces);
+    
+    router.post("/uploadImage", products.uploadImage);
 
   module.exports =router;
