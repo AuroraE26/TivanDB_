ec44cf63eef3aa4c55023452d6779888913f74b9
Ftnt <jairo.rocano@gmail.com>
2022-02-22 02:49:56 -0500

    view image aws metadata

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,7 +1,6 @@
 const products = require("../controller/product.controller.js");
+const upload = require("../lib/s3");
 var router = require("express").Router();
-
-const upload = require("../lib/common");
 
     router.post("/", products.create);
     router.get("/", products.findAll);
@@ -10,7 +9,10 @@ const upload = require("../lib/common");
     router.patch("/:id",products.logicDelete);
     router.patch("/:id/favorito",products.favorite);
     router.patch("/:id/piece",products.pieces);
+
+  
     
     router.post("/uploadImage",upload.single("image"), products.uploadImage);
+    // router.suply("/suply",products.suply)
 
   module.exports =router;
