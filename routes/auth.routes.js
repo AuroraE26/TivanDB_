6e11141fac50e9dbbc5d7eb0ba794b42e9a798b5
Ftnt <jairo.rocano@gmail.com>
2022-02-23 17:32:06 -0500

    feature:more validations were added for the registry and a validationFields middleware was created.

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { check } = require("express-validator");
 const { login, register } = require("../controller/auth.controller.js");
+const { validateFields } = require("../middleware/validateFields.js");
 
 router.get("/", (req, res) => {
   res.json({
@@ -31,7 +32,14 @@ router.get("/", (req, res) => {
 router.post("/login", login);
 router.post(
   "/register",
-  check("email", "No mailing structure").isEmail(),
+  [
+    check("email", "No mailing structure").isEmail(),
+    check("password", "The password must not be empty").not().isEmpty(),
+    check("password", "The password must be at least 6 digits long").isLength({
+      min: 6,
+    }),
+    validateFields,
+  ],
   register
 );
 
