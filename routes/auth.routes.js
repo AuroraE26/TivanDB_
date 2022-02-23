76dfa4a92ff75703e1b1e925a19f7a65501dafdc
Ftnt <jairo.rocano@gmail.com>
2022-02-23 05:34:15 -0500

    fix:add developers

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,32 @@
 const router = require("express").Router();
 const { login, register } = require("../controller/auth.controller.js");
 
+router.get("/", (req, res) => {
+  res.json({
+    Api: "Tivan",
+    version: "0.2.1",
+    developersBack: [
+      {
+        name: "Aurora Escalera",
+        git: "@AuroraE26",
+      },
+      {
+        name: "Jairo Rocano",
+        git: "@Ftnt",
+      },
+    ],
+    developersFront: [
+      {
+        name: "Elfego Aguilar",
+        git: "@EXIDEOR",
+      },
+      {
+        name: "Odon Lozada",
+        git: "@OdonML",
+      },
+    ],
+  });
+});
 router.post("/login", login);
 router.post("/register", register);
 
